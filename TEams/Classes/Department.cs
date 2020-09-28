aee6e467e740f4215da051cfb763369ac7acc434
David Luidens <d.luidens@gmail.com>
2020-09-27 22:55:20 -0400

    Classes updated

--- TEams/Classes/Department.cs
+++ TEams/Classes/Department.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace TEams.Classes
 {
-    class Department
+    public class Department
     {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+
+        public Department(int departmentId, string name)
+        {
+            this.DepartmentId = departmentId;
+            this.Name = name;
+        }
     }
 }
--- TEams/Classes/Project.cs
+++ TEams/Classes/Project.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace TEams.Classes
 {
-    class Project
+    public class Project
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string StartDate { get; set; }
+        public string DueDate { get; set; }
+        public List<Employee> TeamMembers { get; set; }
+
+        public Project(string name, string description, string startDate, string dueDate)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.StartDate = startDate;
+            this.DueDate = dueDate;
+        }
     }
 }
